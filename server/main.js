d23895288429e0ec02af28eb04746430c7a785bf
yimankaing <yimankaing@gmail.com>
2018-08-23 22:36:29 +0700

    add subject find method

--- server/main.js
+++ server/main.js
@@ -4,23 +4,35 @@ import './config/account';
 // import './publication';
 import "../imports/startup/server";
 
+import {SUBJECT} from './methods/subject';
 
 Meteor.startup(() => {
 // Enable cross origin requests for all endpoints
-//   JsonRoutes.setResponseHeaders({
-//     "Cache-Control": "no-store",
-//     "Pragma": "no-cache",
-//     "Access-Control-Allow-Origin": "*",
-//     "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
-//     "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
-//   });
+  JsonRoutes.setResponseHeaders({
+    "Cache-Control": "no-store",
+    "Pragma": "no-cache",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
+  });
 
 
   // if (Meteor.users.find({}).count() === 0) {
-    // Accounts.createUser({
-    //   username: "super",
-    //   email: "email@gmail.com",
-    //   password: "123456"
-    // })
+  // Accounts.createUser({
+  //   username: "super",
+  //   email: "email@gmail.com",
+  //   password: "123456"
+  // })
   // }
+
+
+  JsonRoutes.add("get", "/find_subject/:params", function (req, res, next) {
+    res.charset = "utf-8";
+    const params = req.params.params;
+    console.log(params);
+
+    JsonRoutes.sendResult(res, {
+      data: params ? SUBJECT.findOne(params) : SUBJECT.find()
+    });
+  });
 });
